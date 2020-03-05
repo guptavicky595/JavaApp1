294046e90d41b8668050c82567bb5494ea49659e
guptavicky595 <guptavicky595@gmail.com>
2020-03-05 12:13:22 +0530

    Initial code

--- src/myfirst.java
+++ src/myfirst.java
@@ -4,6 +4,7 @@ public class myfirst {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Helloworld");
+		System.out.println("Welcome to the world of Java");
 	}
 
 }
